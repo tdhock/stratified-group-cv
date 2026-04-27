2a117f77743340c6cb52c5a2134700af5991db45
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-04-27 10:58:49 -0400

    Data gen

--- stratified_atime.R
+++ stratified_atime.R
@@ -1,26 +1,6 @@
-reticulate::use_condaenv("2023-08-deep-learning")
-library(data.table)
 library(ggplot2)
-train_dt <- fread("train.csv")[order(RescuerID)]
-train_dt[1]
-reticulate::py_run_string("from stratified_group import stratified_group_k_fold")
-main <- reticulate::import_main()
-
-zfac <- function(x)as.integer(factor(x))-1L
-main$nfolds <- nfolds <- 5L
-alist <- atime::atime(
-  N=10^seq(1, 6, by=0.2),
-  setup={
-    Ndt <- train_dt[(seq(1, N)-1L) %% .N + 1L][, let(
-      group = zfac(RescuerID),
-      stratum=zfac(AdoptionSpeed)
-    )]
-    main$Ndf <- Ndt
-  },
-  seconds.limit=0.1,
-  cpp=mlr3resampling:::stratified_group_cv_interface(Ndt$stratum, Ndt$group, nfolds),
-  py=reticulate::py_run_string("stratified_group_k_fold(Ndf.stratum.values, Ndf.group.values, nfolds)"),
-  None=reticulate::py_run_string("None"))
+load("stratified_atime.RData")
+requireNamespace("atime")
 alist$unit.col.vec <- c(seconds="median")
 plot(alist)
 
@@ -34,14 +14,14 @@ overhead <- geom_text(aes(
 xlog <- scale_x_log10("N = number of rows input to fold generation function")
 ylog <- scale_y_log10("Time to compute folds with group+strata constraint\nmedian line, min/max band")
 (gg <- plot(apred)+
-   geom_vline(color="grey",xintercept=nrow(train_dt))+
+   geom_vline(color="grey",xintercept=ntrain)+
    ggtitle("Kaggle pet adoption data, 5-fold CV with group+strata constraint")+
    overhead+
    facet_null()+
    ylog+
    geom_text(aes(
-     x=nrow(train_dt), y=0,
-     label=sprintf("rows in CSV=%d", nrow(train_dt))),
+     x=ntrain, y=0,
+     label=sprintf("rows in CSV=%d", ntrain)),
      hjust=1, vjust=-0.5, color="grey50")+
    xlog)
 
@@ -49,25 +29,6 @@ png("stratified_atime_kaggle.png", width=6, height=6, units="in", res=200)
 print(gg)
 dev.off()
 
-set.seed(123)
-n = 1e7
-sim_dt <- data.table(
-  PID = sample(seq(1, n/10), n, replace = TRUE),   # ID for Grouping
-  target = ifelse(rbinom(n, size = 1, prob = 0.2) == 1, "A", "B")
-)[order(PID)]
-alist.sim <- atime::atime(
-  N=10^seq(1, 7, by=0.2),
-  setup={
-    Ndt <- sim_dt[(seq(1, N)-1L) %% .N + 1L][, let(
-      group = zfac(PID),
-      stratum=zfac(target)
-    )]
-    main$Ndf <- Ndt
-  },
-  seconds.limit=0.1,
-  cpp=mlr3resampling:::stratified_group_cv_interface(Ndt$stratum, Ndt$group, nfolds),
-  py=reticulate::py_run_string("stratified_group_k_fold(Ndf.stratum.values, Ndf.group.values, nfolds)"),
-  None=reticulate::py_run_string("None"))
 plot(alist.sim)
 
 arefs.sim <- atime::references_best(alist.sim)
---
+++ stratified_atime_data.R
@@ -0,0 +1,48 @@
+remotes::install_github("tdhock/mlr3resampling@port-py")
+reticulate::use_condaenv("2023-08-deep-learning")
+library(data.table)
+library(ggplot2)
+train_dt <- fread("train.csv")[order(RescuerID)]
+train_dt[1]
+reticulate::py_run_string("from stratified_group import stratified_group_k_fold")
+main <- reticulate::import_main()
+
+zfac <- function(x)as.integer(factor(x))-1L
+main$nfolds <- nfolds <- 5L
+alist <- atime::atime(
+  N=10^seq(1, 6, by=0.2),
+  setup={
+    Ndt <- train_dt[(seq(1, N)-1L) %% .N + 1L][, let(
+      group = zfac(RescuerID),
+      stratum=zfac(AdoptionSpeed)
+    )]
+    main$Ndf <- Ndt
+  },
+  seconds.limit=0.1,
+  cpp=mlr3resampling:::stratified_group_cv_interface(Ndt$stratum, Ndt$group, nfolds),
+  py=reticulate::py_run_string("stratified_group_k_fold(Ndf.stratum.values, Ndf.group.values, nfolds)"),
+  None=reticulate::py_run_string("None"))
+
+set.seed(123)
+n = 1e7
+sim_dt <- data.table(
+  PID = sample(seq(1, n/10), n, replace = TRUE),   # ID for Grouping
+  target = ifelse(rbinom(n, size = 1, prob = 0.2) == 1, "A", "B")
+)[order(PID)]
+alist.sim <- atime::atime(
+  N=10^seq(1, 7, by=0.2),
+  setup={
+    Ndt <- sim_dt[(seq(1, N)-1L) %% .N + 1L][, let(
+      group = zfac(PID),
+      stratum=zfac(target)
+    )]
+    main$Ndf <- Ndt
+  },
+  seconds.limit=0.1,
+  cpp=mlr3resampling:::stratified_group_cv_interface(Ndt$stratum, Ndt$group, nfolds),
+  py=reticulate::py_run_string("stratified_group_k_fold(Ndf.stratum.values, Ndf.group.values, nfolds)"),
+  None=reticulate::py_run_string("None"))
+
+ntrain <- nrow(train_dt)
+save(alist, alist.sim, ntrain, file="stratified_atime.RData")
+
