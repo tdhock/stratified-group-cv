2d7610d1b51bea7f90eb1f56ec70b04fef958cdb
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-04-30 07:08:04 -0400

    compute RSS

--- stratified-group-k-fold-cross-validation.R
+++ stratified-group-k-fold-cross-validation.R
@@ -3,3 +3,4 @@ train_dt <- fread("train_fold.csv")
 train_dt[, table(fold, AdoptionSpeed)]
 (count_dt <- train_dt[, .(N=.N), by=.(RescuerID, fold)][order(N)])
 count_dt[, .(folds=.N), by=RescuerID][, table(folds)]
+cv <- mlr3resampling::ResamplingSameOtherSizesCV$new()
--- train_Task.R
+++ train_Task.R
@@ -1,9 +1,18 @@
 library(data.table)
-train_dt <- fread("train.csv")
+train_dt <- fread("train_fold.csv")
 train_dt[1]
 train_task <- mlr3::as_task_classif(train_dt, target="AdoptionSpeed")
 train_task$col_roles$stratum <- "AdoptionSpeed"
 train_task$col_roles$group <- "RescuerID"
 cv <- mlr3resampling::ResamplingSameOtherSizesCV$new()
+folds <- length(unique(train_dt$fold))
+cv$param_set$values$folds <- folds
 cv$instantiate(train_task)
-cv$instance$fold.dt[, table(stratum, fold)]
+RSS <- function(st, f){
+  stab <- as.numeric(table(st))/folds
+  sftab <- table(st, f)
+  dtab <- stab-sftab
+  sum(dtab^2)
+}
+cv$instance$fold.dt[, RSS(stratum, fold)]
+train_dt[, RSS(AdoptionSpeed, fold)]
