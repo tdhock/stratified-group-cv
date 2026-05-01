473aab2a88ac9eb46f7c89aefd23f1c3fc771ce3
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-05-01 07:00:15 -0400

    control sort

--- train_Task.R
+++ train_Task.R
@@ -14,5 +14,15 @@ RSS <- function(st, f){
   dtab <- stab-sftab
   c(RSS=sum(dtab^2), mean.sd=mean(apply(sftab,1,sd)))
 }
-cv$instance$fold.dt[, RSS(stratum, fold)]
-train_dt[, RSS(AdoptionSpeed, fold)]
+rbind(
+  R=cv$instance$fold.dt[, RSS(stratum, fold)],
+  py=train_dt[, RSS(AdoptionSpeed, fold)])
+
+res_list <- list()
+for(algo in c("RSS", "Wasikowski", "WasikowskiLinearMemory")){
+  fun_name <- paste0("stratified_group_cv_", algo, "_interface")
+  fun <- getFromNamespace(fun_name, "mlr3resampling")
+  fold.vec <- cv$instance$group.row.dt[, fun(stratum-1L, cumsum(c(FALSE, diff(g_ord)!=0)), folds)]+1L
+  res_list[[algo]] <- RSS(cv$instance$group.row.dt$stratum, fold.vec)
+}
+do.call(rbind, res_list)
