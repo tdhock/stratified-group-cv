741a4dfdb36bc875f3b00ede3f1825e62a9da686
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-06-16 15:58:41 -0400

    origami

--- several_Tasks_data.R
+++ several_Tasks_data.R
@@ -1,7 +1,8 @@
 library(data.table)
 RSS <- function(st, f){
+  if(all(is.na(f)))return(c(RSS=NA, RMSE=NA, mean.sd=NA, zeros=NA))
   stab <- as.numeric(table(st))/folds
-  cat(sprintf("folds=%d seed=%d algo=%s\n", folds, seed, algo))
+  cat(sprintf("data=%s folds=%d seed=%d algo=%s\n", data.name, folds, seed, algo))
   print(sftab <- table(st, f))
   dtab <- stab-sftab
   sq.err <- dtab^2
@@ -16,7 +17,7 @@ for(data.i in seq_along(data.csv.vec)){
   train_task <- mlr3::as_task_classif(task_dt, target="target")
   train_task$col_roles$stratum <- "target"
   train_task$col_roles$group <- "groupID"
-  for(folds in 2:10)for(seed in 1:10)for(algo in c("random", "RSS","Wasikowski")){
+  for(folds in 2:10)for(seed in 1:10)for(algo in c("random", "RSS", "Wasikowski", "origami")){
     set.seed(seed)
     fold.dt <- if(algo=="random"){
       unique(task_dt[, .(
@@ -24,6 +25,19 @@ for(data.i in seq_along(data.csv.vec)){
       )])[
       , fold := sample(rep(1:folds, length.out=.N))
       ][task_dt, on="groupID"]
+    }else if(algo=="origami"){
+      tryCatch({
+        fold_list <- with(task_dt, origami::make_folds(
+          cluster_ids=groupID,
+          strata_ids=target,
+          V=folds
+        ))
+        data.table(fold=seq_along(fold_list))[
+        , .(row=fold_list[[fold]]$valid)
+        , by=fold][order(row), data.table(fold, task_dt)]
+      }, error=function(e){
+        data.table(fold=NA_integer_, task_dt)
+      })
     }else{
       cv <- mlr3resampling::ResamplingSameOtherSizesCV$new()
       cv$param_set$values$folds <- folds
