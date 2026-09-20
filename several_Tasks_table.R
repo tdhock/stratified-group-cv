7a28829ca1f13d8bf76f9e10a501bb332d26fe73
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-09-20 15:16:14 -0400

    AIstats

--- several_Tasks_table.R
+++ several_Tasks_table.R
@@ -12,7 +12,8 @@ for(data.name in names(counts.list)){
 out.list
 
 dshow <- counts.long[
-  data.name %in% c("Laribi2024", "PetAdoption")
+  #data.name %in% c("Laribi2024", "PetAdoption")
+  data.name != "five"
 ]
 ideal <- dshow[algo=="RSS", .(
   #total=sum(rows),
@@ -30,10 +31,10 @@ din <- rbind(
     ideal[, .(data.name, stratum, rank)],
     on=.NATURAL
   ][, names(ideal), with=FALSE]
-)[, d := substring(data.name, 1, 1)]
+)[, d := factor(substring(data.name, 1, 1), c("r","A","P","L"))]
 out.dt <- dcast(
-  din,
-  algo + fold ~ d + rank,
+  din[, algo.fold := paste(algo, fold)],
+  algo.fold ~ d + rank,
   value.var="rows")
 library(xtable)
 onames <- grep("_", names(out.dt), value = TRUE)
@@ -41,5 +42,5 @@ xtin <- data.table(out.dt)[, (onames) := lapply(.SD, format), .SDcols=onames]
 xt <- xtable(xtin)
 xt.chr <- print(xt, include.rownames = FALSE)
 out.chr <- gsub("([.]0+)", "\\\\textcolor{white}{\\1}", xt.chr)
-bf.chr <- gsub("(1047|1049|1050|52944|52945|52946)", "\\\\textcolor{red}{\\1}", out.chr)
+bf.chr <- gsub("(47|48|49|50|51|168|169|160|171|172|1047|1049|1050|52944|52945|52946)", "\\\\textcolor{red}{\\1}", out.chr)
 cat(bf.chr)
